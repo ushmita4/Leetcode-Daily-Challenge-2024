e134106c1cf301894984cce4d7c601b841b84488
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2024-04-10 21:52:15 +0530

    Create revealcardsinincreasingorder.cpp

---
+++ 04. April/revealcardsinincreasingorder.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> merge(vector<int>& left, vector<int>& right) {
+        int merged_size = left.size() + right.size();
+        if (merged_size % 2 == 1) {
+            int last_element = right[right.size() - 1];
+            for (int i = right.size() - 1; i > 0; i--) {
+                right[i] = right[i - 1];
+            }
+            right[0] = last_element; 
+        }
+        vector<int> merged(merged_size);
+        int left_index = 0, right_index = 0, merged_index = 0;
+
+        while (left_index < left.size() && right_index < right.size()) {
+            merged[merged_index++] = left[left_index++];
+            merged[merged_index++] = right[right_index++];
+        }
+        while (left_index < left.size()) {
+            merged[merged_index++] = left[left_index++];
+        }
+        while (right_index < right.size()) {
+            merged[merged_index++] = right[right_index++];
+        }
+        return merged; 
+    }
+
+    vector<int> divide(vector<int>& array, int start, int end) {
+        if (start > end) return {}; // Base case: empty subarray
+        if (start == end) return {array[start]}; // Base case: single element subarray
+        // Calculate the middle index of the current subarray
+        int mid = start + (end - start) / 2;
+        // Recursively divide the left half of the array
+        vector<int> left_half;
+        for(int i = start; i <= mid; i++){
+            left_half.push_back(array[i]);
+        }
+        // Recursively divide the right half of the array
+        vector<int> right_half = divide(array, mid + 1, end);
+        return merge(left_half, right_half);
+    }
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+        return divide(deck, 0, deck.size() - 1);
+    }
+};

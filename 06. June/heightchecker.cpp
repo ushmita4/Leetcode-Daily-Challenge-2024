0cd3933c5b3451aaaae794d5eb635ae3cb1bbded
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2024-06-10 21:11:05 +0530

    Create heightchecker.cpp

---
+++ 06. June/heightchecker.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> exp=heights;
+        sort(exp.begin(),exp.end());
+        int c=0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=exp[i])
+            c++;
+        }
+        return c;
+    }
+};

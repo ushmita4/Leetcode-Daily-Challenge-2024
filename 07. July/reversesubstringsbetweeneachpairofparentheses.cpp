c93325e827367b42956decb70b955336b8cbfd77
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2024-07-11 22:17:59 +0530

    Create reversesubstringsbetweeneachpairofparentheses.cpp

---
+++ 07. July/reversesubstringsbetweeneachpairofparentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> openParenthesesIndices;
+        string result;
+        for (char currentChar : s) {
+            if (currentChar == '(') {
+                openParenthesesIndices.push(result.length());
+            } else if (currentChar == ')') {
+                int start = openParenthesesIndices.top();
+                openParenthesesIndices.pop();
+                
+                reverse(result.begin() + start, result.end());
+            } else {
+                
+                result += currentChar;
+            }
+        }
+        return result;
+    }
+};

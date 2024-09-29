4c2bce718640a23ea4340c1983ab96968e3574ca
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2024-09-29 22:20:22 +0530

    Create allo'onedatastructure.cpp

---
+++ 09. September/allo'onedatastructure.cpp
@@ -0,0 +1,140 @@
+class Node {
+public:
+    int freq;
+    Node* prev;
+    Node* next;
+    unordered_set<string> keys;
+
+    Node(int freq) : freq(freq), prev(nullptr), next(nullptr) {}
+};
+
+class AllOne {
+private:
+    Node* head;                        
+    Node* tail;                       
+    unordered_map<string, Node*> map;  
+
+public:
+    
+    AllOne() {
+        head = new Node(0); 
+        tail = new Node(0); 
+        head->next = tail;   
+        tail->prev = head;  
+    }
+
+   
+    void inc(string key) {
+        if (map.find(key) != map.end()) {
+            Node* node = map[key];
+            int freq = node->freq;
+            node->keys.erase(key);  // Remove key from current node
+
+            Node* nextNode = node->next;
+            if (nextNode == tail || nextNode->freq != freq + 1) {
+                // Create a new node if next node does not exist or freq is not
+                // freq + 1
+                Node* newNode = new Node(freq + 1);
+                newNode->keys.insert(key);
+                newNode->prev = node;
+                newNode->next = nextNode;
+                node->next = newNode;
+                nextNode->prev = newNode;
+                map[key] = newNode;
+            } else {
+                // Increment the existing next node
+                nextNode->keys.insert(key);
+                map[key] = nextNode;
+            }
+
+            // Remove the current node if it has no keys left
+            if (node->keys.empty()) {
+                removeNode(node);
+            }
+        } else {  // Key does not exist
+            Node* firstNode = head->next;
+            if (firstNode == tail || firstNode->freq > 1) {
+                // Create a new node
+                Node* newNode = new Node(1);
+                newNode->keys.insert(key);
+                newNode->prev = head;
+                newNode->next = firstNode;
+                head->next = newNode;
+                firstNode->prev = newNode;
+                map[key] = newNode;
+            } else {
+                firstNode->keys.insert(key);
+                map[key] = firstNode;
+            }
+        }
+    }
+
+    // Decrements an existing key by 1. If Key's value is 1, remove it from the
+    // data structure.
+    void dec(string key) {
+        if (map.find(key) == map.end()) {
+            return;  // Key does not exist
+        }
+
+        Node* node = map[key];
+        node->keys.erase(key);
+        int freq = node->freq;
+
+        if (freq == 1) {
+            // Remove the key from the map if freq is 1
+            map.erase(key);
+        } else {
+            Node* prevNode = node->prev;
+            if (prevNode == head || prevNode->freq != freq - 1) {
+                // Create a new node if the previous node does not exist or freq
+                // is not freq - 1
+                Node* newNode = new Node(freq - 1);
+                newNode->keys.insert(key);
+                newNode->prev = prevNode;
+                newNode->next = node;
+                prevNode->next = newNode;
+                node->prev = newNode;
+                map[key] = newNode;
+            } else {
+                // Decrement the existing previous node
+                prevNode->keys.insert(key);
+                map[key] = prevNode;
+            }
+        }
+
+        // Remove the node if it has no keys left
+        if (node->keys.empty()) {
+            removeNode(node);
+        }
+    }
+
+    // Returns one of the keys with maximal value.
+    string getMaxKey() {
+        if (tail->prev == head) {
+            return "";  // No keys exist
+        }
+        return *(tail->prev->keys.begin());  // Return one of the keys from the
+                                             // tail's previous node
+    }
+
+    // Returns one of the keys with minimal value.
+    string getMinKey() {
+        if (head->next == tail) {
+            return "";  // No keys exist
+        }
+        return *(
+            head->next->keys
+                .begin());  
+    }
+
+private:
+    void removeNode(Node* node) {
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
+
+        prevNode->next = nextNode;  
+        nextNode->prev = prevNode; 
+
+        delete node;  
+    }
+};
